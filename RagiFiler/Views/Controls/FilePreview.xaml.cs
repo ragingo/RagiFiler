65d5e6d264588cdac3f12c6ec9893b0bd3b8a9a7
ragingo <save.the.world.crush@gmail.com>
2020-01-15 23:58:14 +0900

    VisualTreeHelper使いにくいからやり方変えた

--- RagiFiler/Views/Controls/FilePreview.xaml.cs
+++ RagiFiler/Views/Controls/FilePreview.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RagiFiler.Views.Controls
 {
@@ -27,6 +20,19 @@ namespace RagiFiler.Views.Controls
             {
                 return;
             }
+
+            var parent = mediaElement.Parent;
+
+            var slider = parent.GetChildren().OfType<Slider>().FirstOrDefault();
+            if (slider != null)
+            {
+                slider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+                slider.ValueChanged += OnSliderValueChanged;
+            }
+        }
+
+        private void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
         }
     }
 
